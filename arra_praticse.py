82a85c6027a142af41bb163ac267eed201064244
vaibhav-cmyk <vaibhavatiwadkar7338@gmail.com>
2025-07-25 10:56:05 +0530

    done

---
+++ arra_praticse.py
@@ -0,0 +1,165 @@
+from numpy import *
+
+
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
+
+ 
+
+# Example 1:
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+# Example 2:
+
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+# Example 3:
+
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+ 
+
+# Constraints:
+
+# 2 <= nums.length <= 104
+# -109 <= nums[i] <= 109
+# -109 <= target <= 109
+# Only one valid answer exists.
+
+
+# num = [3,2,4]
+# target = 6
+# for i in range(0,len(num)):
+#     for j in range(i+1,len(num)):
+#         if num[i] + num[j] == target:
+#             print(i,j)
+#             j+=1
+#     i+=1        
+# _____________________________________
+
+
+# Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+# The overall run time complexity should be O(log (m+n)).
+
+# Example 1:
+# Input: nums1 = [1,3], nums2 = [2]
+# Output: 2.00000
+# Explanation: merged array = [1,2,3] and median is 2.
+
+# Example 2:
+# Input: nums1 = [1,2], nums2 = [3,4]
+# Output: 2.50000
+# Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+
+# Constraints:
+# nums1.length == m
+# nums2.length == n
+# 0 <= m <= 1000
+# 0 <= n <= 1000
+# 1 <= m + n <= 2000
+# -106 <= nums1[i], nums2[i] <= 106
+
+# num1 = [1,2]
+# num2 = [3,4]
+
+# output = num1 + num2
+
+# sort(output)
+
+# print(median(output))
+
+
+
+# 3)Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+# Notice that the solution set must not contain duplicate triplets.
+
+# Example 1:
+
+# Input: nums = [-1,0,1,2,-1,-4]
+# Output: [[-1,-1,2],[-1,0,1]]
+# Explanation: 
+# nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+# nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+# nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+# The distinct triplets are [-1,0,1] and [-1,-1,2].
+# Notice that the order of the output and the order of the triplets does not matter.
+
+# num = [0,0,0]
+
+# for i in range(0,len(num)):
+#     for j in range(i+1,len(num)):
+#         for k in range(j+1,len(num)):
+#             if num[i]+num[j]+num[k] == 0:
+#                 print([num[i],num[j],num[k]])
+#             else:
+#                 print('[]')      
+
+# 4)
+# Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+# Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+# Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+# Return k.
+# Custom Judge:
+
+# The judge will test your solution with the following code:
+
+# int[] nums = [...]; // Input array
+# int[] expectedNums = [...]; // The expected answer with correct length
+
+# int k = removeDuplicates(nums); // Calls your implementation
+
+# assert k == expectedNums.length;
+# for (int i = 0; i < k; i++) {
+#     assert nums[i] == expectedNums[i];
+# }
+# If all assertions pass, then your solution will be accepted.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,1,2]
+# Output: 2, nums = [1,2,_]
+# Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+# It does not matter what you leave beyond the returned k (hence they are underscores).
+# Example 2:
+
+# Input: nums = [0,0,1,1,1,2,2,3,3,4]
+# Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+# Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+# It does not matter what you leave beyond the returned k (hence they are underscores).
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 3 * 104
+# -100 <= nums[i] <= 100
+# nums is sorted in non-decreasing order.
+
+# num = [0,0,1,1,1,2,2,3,3,4]
+
+# print(set(num))
+    
+#  Example 1:
+
+# Input: nums = [3,2,2,3], val = 3
+# Output: 2, nums = [2,2,_,_]
+# Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+# It does not matter what you leave beyond the returned k (hence they are underscores).
+# Example 2:
+
+# Input: nums = [0,1,2,2,3,0,4,2], val = 2
+# Output: 5, nums = [0,1,4,0,3,_,_,_]
+# Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+# Note that the five elements can be returned in any order.
+# It does not matter what you leave beyond the returned k (hence they are underscores).             
+
+   
