f1f6b63080f4cc736448adf7c61329a9ba46e7bc
vaibhav-cmyk <vaibhavatiwadkar7338@gmail.com>
2025-07-25 18:50:30 +0530

    done

---
+++ arraymatrix.py
@@ -0,0 +1,39 @@
+from numpy import *
+
+# num = array([
+#     [1,2,3],
+#     [4,5,6]
+# ])
+# m1 = matrix('1,2,3,4')
+# m2 = matrix('5,6,7,8')
+
+# print(m)
+# print()
+# print('diagonals are:')
+# print(m.max())
+
+# m3 = m1 + m2
+# print(m3)
+
+# num = array([1,2])
+# num1 = array([3,4])
+
+# def greet():
+#     a = int(input('enter value for a:'))
+#     b = int(input('enter value for b:'))
+
+#     result=a+b
+#     result1 = a-b
+#     return result, result1
+
+# result,result1 = greet()
+# print(result)
+# print(result1)
+
+
+
+
+
+
+
+
