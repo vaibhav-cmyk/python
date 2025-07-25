5c432ca241aba64440ee2d7c931e99ad309bcd6d
vaibhav-cmyk <vaibhavatiwadkar7338@gmail.com>
2025-07-25 08:23:45 +0530

    done

---
+++ 24thjuly.py
@@ -0,0 +1,39 @@
+from numpy import *
+
+
+# num = array([1,2,3,4,5])
+# print(num)
+# for i in range(0,len(num)):
+#     sum = num[i]+5
+#     print(sum)
+
+# i = 0
+# while i < len(num):
+#     sum = num[i] + 5
+#     print(sum)
+#     i+=1
+
+# result = num + 5
+# print(result)
+
+
+num1 = array([11,2,1,4,5])
+num2 = array([1,2,3,4,5])
+
+# print(sin(num1))
+# print(sqrt(num1))
+# print(sin(num1))
+
+# print(sort(num1))
+
+# add=num1 + 5
+# print(add(0,len(num1)))
+
+# print(mean(num1))
+
+sm = concatenate([num1,num2])
+
+print(sm)
+print(sort(sm))
+
+
